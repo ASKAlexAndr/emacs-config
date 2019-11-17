0154a20a215b1234a35f6f551dd2de4eb3283cd0
ASKAlexAndr <ask.97.alexandr@gmail.com>
2019-11-17 01:20:06 +0000

    fix keybindings

---
+++ modules/keybindings.el
@@ -0,0 +1,9 @@
+;; keybindings
+(global-unset-key (kbd "C-i"))
+(global-set-key (kbd "C-i") 'previous-line)
+(global-unset-key (kbd "C-k"))
+(global-set-key (kbd "C-k") 'next-line)
+(global-unset-key (kbd "C-j"))
+(global-set-key (kbd "C-j") 'backward-char)
+(global-unset-key (kbd "C-l"))
+(global-set-key (kbd "C-l") 'forward-char)
